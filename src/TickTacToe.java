e885e52356c22b10a9ceaea0916a1312bf2c9724
Poojanavgurukul <Pooja17@navgurukul.org>
2019-06-03 11:27:26 +0530

    Refactor.make a function of showing gird

--- src/TickTacToe.java
+++ src/TickTacToe.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class TickTacToe {
     public static void main(String[] args) {
         Scanner inputObj=new Scanner(System.in);
-
+        displayBoard();
+    }
+    public static String[][]displayBoard(){
         String [][]board=new String[][]{
                 {"1","2","3"},
                 {"4","5","6"},
@@ -16,14 +18,6 @@ public class TickTacToe {
                 System.out.print(number1+"|");
             }
             System.out.print("\n");
-        }
-        for (int i = 0; i <board.length ; i++) {
-            for (int j = 0; j <board.length ; j++) {
-                System.out.println("Enter x ");
-                String player1=inputObj.nextLine();
-                System.out.println("Enter o");
-                String player2=inputObj.nextLine();
-            }
-        }
+        }return board;
     }
 }
