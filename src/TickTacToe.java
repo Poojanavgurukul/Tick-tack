4f2240384d95d4c2a049cf671c707ef994ee293d
Poojanavgurukul <Pooja17@navgurukul.org>
2019-06-03 14:29:35 +0530

    Refactor.variable name number to element and number1 to element1

--- src/TickTacToe.java
+++ src/TickTacToe.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TickTacToe {
     public static void main(String[] args) {
         Scanner inputObj=new Scanner(System.in);
-        displayBoard();
+        replace(displayBoard(),inputObj);
     }
     public static String[][]displayBoard(){
         String [][]board=new String[][]{
@@ -13,11 +13,21 @@ public class TickTacToe {
                 {"4","5","6"},
                 {"7","8","9"}
         };
-        for (String[] number:board) {
-            for (String number1:number) {
-                System.out.print(number1+"|");
+        for (String[] element:board) {
+            for (String element1:element) {
+                System.out.print(element1+"|");
             }
             System.out.print("\n");
         }return board;
     }
+    public static String[][] replace(String[][]board,Scanner inputObj){
+        for (int i = 0; i <board.length ; i++) {
+            for (int j = 0; j <board.length ; j++) {
+                System.out.println(board[i][j]);
+               // System.out.println("enter x");
+               // String player=inputObj.nextLine();
+            }
+        }return board;
+    }
+
 }
